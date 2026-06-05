28142e7f6c8c7e2bc3175eda939a55cde35e018c
Varshanth123 <mehendhikarvarshanth@gmail.com>
2026-06-05 23:06:59 +0530

    second commit

---
+++ 1-Python_Basics/Operators_in_Python/Relational_operators.py
@@ -0,0 +1,18 @@
+# Relational operator
+a=int(input("Enter A value "))
+b=int(input("Enter B value "))
+
+# > greater than 
+print("Is A is greater than B? ",a>b)
+
+# >= greater than or equal to 
+print("Is A is greater than or equal to B? ",a>=b)
+
+#<=lesser then or equal to 
+print("Is A is lesser than or equal to B? ",a<=b)
+
+# == equal or not
+print("Is A is equal to B? ",a==b)
+
+# != is not equal to or not 
+print("IS A is notequal to B? ",a!=b)
