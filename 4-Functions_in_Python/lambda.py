726a8a989bb5533dedc86ea41dd94649d04d76e4
Varshanth123 <mehendhikarvarshanth@gmail.com>
2026-06-08 00:06:19 +0530

    8th june 2026

--- 4-Functions_in_Python/lambda.py
+++
@@ -1,2 +0,0 @@
-add=lambda x,y:x+y
-print(add(10,20))
---
+++ 5-Importing_and_Creating_Modules_and_Packages/test.py
@@ -0,0 +1,12 @@
+import calculator
+
+print(calculator.addition(5, 3))
+print(calculator.subtraction(5, 3))
+print(calculator.difference(5, 3))
+
+'''
+Output:
+8
+2
+2
+'''
